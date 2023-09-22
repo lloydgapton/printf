564e997017c3f6bb8cc68e0c452370c69aec617a
lloydgapton <lloydgapton123@gmail.com>
2023-09-22 14:53:36 -0700

    updated file

--- _putchar.c
+++ _putchar.c
@@ -14,7 +14,7 @@ int _putchar(char ch)
 		contador = 0;
 		return (0);
 	}
-	if (ch == -2 || contandor == 1024)
+	if (ch == -2 || contador == 1024)
 	{
 		write(1, buffer, contador);
 		contador = 0;
