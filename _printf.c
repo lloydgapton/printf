271db53b73358d0bf1088466646e46f0abf3deb9
lloydgapton <108632761+lloydgapton@users.noreply.github.com>
2023-09-22 23:49:46 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -47,3 +47,11 @@ int _printf(char *format, ...)
 	_putchar(-2);
 	return (written);
 }
+int main(void)
+{
+	_printf("Lloyd\n");
+	_printf("%c\n", 'v');
+	_printf("%s\n", "string");
+	_printf("%%\n");
+	return (0);
+}
